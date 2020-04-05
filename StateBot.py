cb21eeef56e1b7baecfc6ff716d9cd06b226b251
Tristan B. Kildaire <deavmi@disroot.org>
2020-04-05 23:48:52 +0200

    Added code to get configurationn parameters from environment variable argument.

---
+++ StateBot.py
@@ -0,0 +1,45 @@
+import os
+
+# Returns the bot parameters
+def getConfiguration():
+    # Get the collectd server address
+    collectdAddress = os.getenv("collectdAddress")
+
+    # Check if the `collectdAddress` environment variable
+    # exists.
+    if collectdAddress == None:
+        print("Missing collectd server address")
+        return None
+
+    # Get the collectd server port and check if the
+    # `collectdPort` environment variable exists
+    # and that it is an integer.
+    try:
+        collectdPort = os.getenv("collectdPort")
+        if collectdPort == None:
+            print("Missing collectd server port")
+            return None
+        collectdPort = int(collectdPort)
+    except ValueError:
+        print("An integer for the collectd server port is needed")
+        return None
+    
+    
+    return {"address" : collectdAddress, "port" : collectdPort}
+
+# Runs the bot code
+def botStart():
+    pass
+
+# Initializes the bot and starts it
+def init():
+    # Get the configuration and check if it is valid
+    configuration = getConfiguration()
+
+    if configuration == None:
+        print("Exiting, invalid configuration...")
+        exit()
+    else:
+        botStart()
+
+init()
